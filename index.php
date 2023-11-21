a55b5884eb103c591ec35671599a4acd5753eda4
Jonathan-Uloth <146176574+Jonathan-Uloth@users.noreply.github.com>
2023-11-21 09:41:46 +0100

    PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zLynx</title>
+</head>
+<body>
+    <h1>zLynx</h1>
+    <p>
+        Willkommen auf der Webseite von Zlynx.
+    </p>
+</body>
+</html>
